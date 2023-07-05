c147c98c9882ec1237b19924a05bc0618e0da904
phu <phutq0@gmail.com>
2023-07-06 06:08:21 +0700

    base

--- base.py
+++ base.py
@@ -1,7 +1,33 @@
 
+class Button:
+    def __init__(self, name: str = None, x=0, y=0):
+        self.name = name
+        self.x = x
+        self.y = y
+
+    def press():
+        pass
+
+
+class Input:
+    def __init__(self, name: str = None, x=0, y=0):
+        self.name = name
+        self.x = x
+        self.y = y
+
+    def clear():
+        pass
+
+    def type(s: str):
+        pass
+
 
 class Emulator:
-    def __init__(self, port, host="localhost", name=None):
+    """
+        Emulator Instance
+    """
+
+    def __init__(self, port: str, host: str = "localhost", name: str = None):
         self.host = host
         self.port = port
         self.name = name
